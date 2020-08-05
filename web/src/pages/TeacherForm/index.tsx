b71e65aaf15078bdab319832c763b836b1260fd2
thiagorcode_n <54317829+thiagorcoden12@users.noreply.github.com>
2020-08-05 01:15:42 -0300

    Day 1

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader'
 
 function TeacherForm() {
   return (
-    <h1>TeacherList</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas" />
+
+    </div>
   )
 }
 export default TeacherForm;
